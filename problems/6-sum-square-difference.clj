51e6092f7defb18bed421a57940873829b5684cf
CarnunMP <admin@carnun.com>
2020-03-10 17:43:41 +0000

    Added 'The problem: ...' to #6.

--- problems/6-sum-square-difference.clj
+++ problems/6-sum-square-difference.clj
@@ -1,3 +1,12 @@
+;;; The problem: The sum of the squares of the first ten natural numbers is,
+;;;     1^2 + 2^2 + ... + 10^2 = 385
+;;; The square of the sum of the first ten natural numbers is,
+;;;     (1 + 2 + ... + 10)^2 = 55^2 = 3025
+;;; Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is
+;;;     3025 − 385 = 2640.
+;;;
+;;; Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
 (defn sum-squares
   ([n] (sum-squares n 0))
   ([n result]
